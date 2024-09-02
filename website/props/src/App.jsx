41247e1795562fbd262ac3fb30f6fec6c1eff09c
Jadline <njerijadline@gmail.com>
2024-09-02 09:31:14 +0300

    props are read-only properties shared between components

---
+++ website/props/src/App.jsx
@@ -0,0 +1,17 @@
+import Student from "./Student.jsx"
+
+function App() {
+  return (
+    <>
+      <Student name="SpongeBob" age={30} isStudent={true}/>
+      <Student name="Juliet" age={25} isStudent={false}/>
+      <Student name ="Njeri" age={24} isStudent ={true}/>
+      <Student />
+    </>
+  
+  )
+  
+  
+}
+
+export default App
