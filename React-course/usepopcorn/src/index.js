4be9deac916a14b7c36d112c7880692263191265
Jadline <njerijadline@gmail.com>
2024-09-26 12:28:32 +0300

    updating changes on the star component

---
+++ React-course/usepopcorn/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+import StarRating from './starRating';
+import {useState} from "react"
+
+function Test(){
+  const[movieRating,setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onsetRating = {setMovieRating}/>
+      <p>This movie was rated {movieRating} starts</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={5} className ="test" messages = {['Terrible','Bad','Okay','good','amazing']} defaultRating={3}/>
+    <Test/>
+    
+  </React.StrictMode>
+);
+
